6e35be26d8234b83bcc790caf6886f6b5af72a1d
Kritika Sharma <krsharm@microsoft.com>
2021-04-11 00:03:59 +0530

    API Automation Feature file and code updates

---
+++ BDDTestAutomation/EShopApplicationWebSolution/Features/ApiTests/EShopCatalogAPI.feature
@@ -0,0 +1,24 @@
+@ui @owner=kritsha @web @testplan= @testsuite=
+Feature: Login_EShopApplication
+	As a user, I want to be able to login to the EShop application
+
+@bvt @priority=1
+Scenario: Verify that the user is able to get the Catalog Types by calling Catalog API
+	Given user wants to get Catalog types
+	When "catalog types" API is called
+	Then the api should return "200" response
+	And the user should be able to get catalog types
+
+@bvt @priority=1
+Scenario Outline: Verify that the user is able to get the catalog items by calling Catalog API
+	Given user wants to get catalog items
+	When the page index is <pageIndex>
+	And the page size is <pageSize>
+	When "catalog items" API is called
+	Then the api should return "200" response
+	And the user should be able to get catalog items for the given page
+
+	Examples: 
+	| pageIndex | pageSize |
+	| 0         | 10       |
+	| 1         | 5        |
---
+++ BDDTestAutomation/EShopApplicationWebSolution/StepDefinitions/ApiTests/EShopCatalogAPISteps.cs
@@ -0,0 +1,80 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="EShopCatalogAPISteps.cs" company="Microsoft">
+//    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+//    THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+//    OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+//    ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+//    OTHER DEALINGS IN THE SOFTWARE.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace EShopApplicationWebSolution.StepDefinitions.ApiTests
+{
+    using System;
+    using System.Configuration;
+    using System.Net.Http;
+    using Bdd.Core.Api.StepDefinitions;
+    using Newtonsoft.Json.Linq;
+    using NUnit.Framework;
+    using TechTalk.SpecFlow;
+
+    [Binding]
+    public class EShopCatalogAPISteps : ApiStepDefinitionBase
+    {
+        private string endpoint;
+        private string url;
+        private HttpResponseMessage httpResponse;
+        private JObject responseObject;
+
+        [Given(@"user wants to get Catalog types")]
+        [Given(@"user wants to get catalog items")]
+        public void GivenUserWantsToGetCatalog()
+        {
+            this.endpoint = ConfigurationManager.AppSettings.Get("EShopEndpoint");
+        }
+
+        [When(@"""(.*)"" API is called")]
+        public async System.Threading.Tasks.Task WhenAPIIsCalledAsync(string apiName)
+        {
+            this.url = this.endpoint + ConfigurationManager.AppSettings.Get(apiName.Replace(" ", ""));
+
+            this.httpResponse = await this.ApiExecutor.GetResponseAsync(this.url, this.UserDetails);
+
+            this.ScenarioContext["responsestatuscode"] = (int)this.httpResponse.StatusCode;
+            this.ScenarioContext["jsonResponse"] = JObject.Parse(await this.httpResponse.Content.ReadAsStringAsync());
+            this.responseObject = (JObject)this.ScenarioContext["jsonResponse"];
+        }
+
+        [When(@"the page index is (.*)")]
+        public void WhenThePageIndexIs(int pageIndex)
+        {
+            
+        }
+
+        [When(@"the page size is (.*)")]
+        public void WhenThePageSizeIs(int pageSize)
+        {
+            
+        }
+
+        [Then(@"the api should return ""(.*)"" response")]
+        public void ThenTheApiShouldReturnResponse(int httpResponseCode)
+        {
+            Assert.AreEqual(httpResponseCode, this.ScenarioContext["responsestatuscode"]);
+        }
+
+        [Then(@"the user should be able to get catalog types")]
+        public void ThenTheUserShouldBeAbleToGetCatalogTypes()
+        {
+            
+        }
+
+        [Then(@"the user should be able to get catalog items for the given page")]
+        public void ThenTheUserShouldBeAbleToGetCatalogItemsForTheGivenPage()
+        {
+            
+        }
+    }
+}
